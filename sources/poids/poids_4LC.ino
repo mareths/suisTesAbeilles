7fb84995c37d251a1a0d121651bae5afb0f98f93
Ouvriere <ouvriere@ruche.fr>
2016-10-16 14:30:19 +0200

    ajout du prog pour la balance

---
+++ sources/poids/poids_4LC.ino
@@ -0,0 +1,27 @@
+#include "HX711.h"
+
+// Entree digitale
+// OUT	        - pin 3
+// Clock (SDK)	- pin 2
+
+HX711 scale(3, 2);
+
+void setup() {
+// pour suivre sur la sortie (a desactiver...?)  
+  Serial.begin(38400);
+// on va tarer la balance à l'initialisation du prog  (bin oui, comme toute balance)
+// avec la valeur trouvee selon la methode de la lib (differente pour chacun des modeles de celllule)
+  scale.set_scale(-24000.f);
+  scale.tare();
+}
+
+void loop() {
+// on prend 10 mesure et on fait la moyenne
+  Serial.println(scale.get_units(10), 1);
+// on met en sommeil l'ampli
+  scale.power_down();
+// en sommeil pendant (en ms)
+  delay(5000);
+// on reveille pour prendre les mesures
+  scale.power_up();
+}
