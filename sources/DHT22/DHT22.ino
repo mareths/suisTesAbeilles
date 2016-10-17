140ee5bccfbcd1c4a2419dbca7e843939692933e
mareths <sfret44@gmail.com>
2016-10-17 21:31:40 +0200

    Ajout du code source pour le DHT22

--- sources/DHT22/DHT22.ino
+++ sources/DHT22/DHT22.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
+// Ajouter la librairie via Croquis/Inclure une librairie/Gerer les bibliothéques, et chercher dht22
+#include "DHT.h"
+#define DHTPIN 13 // broche ou l'on a branche le capteur
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
 
+DHT dht(DHTPIN, DHTTYPE);//déclaration du capteur
+ 
+void setup()
+{
+ Serial.begin(9600);
+ Serial.println("DHTxx test!");
+ dht.begin();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+ delay(2000);
+ 
+ // La lecture du capteur prend 250ms
+ // Les valeurs lues peuvet etre vieilles de jusqu'a 2 secondes (le capteur est lent)
+ float h = dht.readHumidity();//on lit l'hygrometrie
+ float t = dht.readTemperature();//on lit la temperature en celsius (par defaut)
+ // pour lire en farenheit, il faut le paramère (isFahrenheit = true) :
+ float f = dht.readTemperature(true);
+ 
+ //On verifie si la lecture a echoue, si oui on quitte la boucle pour recommencer.
+ if (isnan(h) || isnan(t) || isnan(f))
+ {
+   Serial.println("Failed to read from DHT sensor!");
+   return;
+ }
+ 
+ // Calcul de l'indice de temperature en Farenheit
+ float hif = dht.computeHeatIndex(f, h);
+ // Calcul de l'indice de temperature en Celsius
+ float hic = dht.computeHeatIndex(t, h, false);
+ 
+ //Affichages :
+ Serial.print("Humidite: ");
+ Serial.print(h);
+ Serial.print(" %\t");
+ Serial.print("Temperature: ");
+ Serial.print(t);
+ Serial.print(" *C ");
+ Serial.print(f);
+ Serial.print(" *F\t");
+ Serial.print("Indice de temperature: ");
+ Serial.print(hic);
+ Serial.print(" *C ");
+ Serial.print(hif);
+ Serial.println(" *F");
 }
