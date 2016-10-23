b021a91c46cbd46132d7bd83ee19d87ce3c59ac0
mareths <sfret44@gmail.com>
2016-10-24 00:43:15 +0200

    ajout d'un exemple plus simple pour le DS18B20

--- sources/example/DS18B20/DS18B20.ino
+++ sources/example/DS18B20/DS18B20.ino
@@ -18,6 +18,10 @@ DallasTemperature sensors(&oneWire);
 // arrays to hold device addresses
 DeviceAddress sondeNumero0, sondeNumero1;
 
+// global variable for temp0 and temp1
+float temp0 = 0;
+float temp1 = 1;
+
 void setup(void)
 {
   // start serial port
@@ -34,101 +38,12 @@ void setup(void)
 
   // search for devices on the bus and assign based on an index.
   if (!sensors.getAddress(sondeNumero0, 0)) Serial.println("Unable to find address for Device 0"); 
-  if (!sensors.getAddress(sondeNumero1, 1)) Serial.println("Unable to find address for Device 1"); 
+  if (!sensors.getAddress(sondeNumero1, 1)) Serial.println("Unable to find address for Device 1");
 
-  // show the addresses we found on the bus
-  Serial.print("Device 0 Address: ");
-  printAddress(sondeNumero0);
-  Serial.println();
+  // Sensibilty set at 0.0625°C
+  sensors.setResolution(sondeNumero0, 12);
+  sensors.setResolution(sondeNumero1, 12);
 
-  Serial.print("Device 0 Alarms: ");
-  printAlarms(sondeNumero0);
-  Serial.println();
-  
-  Serial.print("Device 1 Address: ");
-  printAddress(sondeNumero1);
-  Serial.println();
-
-  Serial.print("Device 1 Alarms: ");
-  printAlarms(sondeNumero1);
-  Serial.println();
-  
-  Serial.println("Setting alarm temps...");
-
-  // alarm when temp is higher than -25C
-  sensors.setHighAlarmTemp(sondeNumero0, -25);
-  
-  // alarm when temp is lower than 50C
-  sensors.setLowAlarmTemp(sondeNumero0, 50);
-  
-  // alarm when temp is higher than -25C
-  sensors.setHighAlarmTemp(sondeNumero1, -25);
-  
-  // alarn when temp is lower than 50C
-  sensors.setLowAlarmTemp(sondeNumero1, 50);
-  
-  Serial.print("New Device 0 Alarms: ");
-  printAlarms(sondeNumero0);
-  Serial.println();
-  
-  Serial.print("New Device 1 Alarms: ");
-  printAlarms(sondeNumero1);
-  Serial.println();
-}
-
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress)
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-  }
-}
-
-// function to print the temperature for a device
-void printTemperature(DeviceAddress deviceAddress)
-{
-  float tempC = sensors.getTempC(deviceAddress);
-  Serial.print("Temp C: ");
-  Serial.print(tempC);
-  Serial.print(" Temp F: ");
-  Serial.print(DallasTemperature::toFahrenheit(tempC));
-}
-
-void printAlarms(uint8_t deviceAddress[])
-{
-  char temp;
-  temp = sensors.getHighAlarmTemp(deviceAddress);
-  Serial.print("High Alarm: ");
-  Serial.print(temp, DEC);
-  Serial.print("C/");
-  Serial.print(DallasTemperature::toFahrenheit(temp));
-  Serial.print("F | Low Alarm: ");
-  temp = sensors.getLowAlarmTemp(deviceAddress);
-  Serial.print(temp, DEC);
-  Serial.print("C/");
-  Serial.print(DallasTemperature::toFahrenheit(temp));
-  Serial.print("F");
-}
-
-// main function to print information about a device
-void printData(DeviceAddress deviceAddress)
-{
-  Serial.print("Device Address: ");
-  printAddress(deviceAddress);
-  Serial.print(" ");
-  printTemperature(deviceAddress);
-  Serial.println();
-}
-
-void checkAlarm(DeviceAddress deviceAddress)
-{
-  if (sensors.hasAlarm(deviceAddress))
-  {
-    Serial.print("ALARM: ");
-    printData(deviceAddress);
-  }
 }
 
 void loop(void)
@@ -138,28 +53,20 @@ void loop(void)
   Serial.print("Requesting temperatures...");
   sensors.requestTemperatures();
   Serial.println("DONE");
+  // delay for conversion due to sensibility (i don't know where we should put this delay...)
+  delay(800);
 
-  // Method 1:
-  // check each address individually for an alarm condition
-  checkAlarm(sondeNumero0);
-  checkAlarm(sondeNumero1);
-/*
-  // Alternate method:
-  // Search the bus and iterate through addresses of devices with alarms
-  
-  // space for the alarm device's address
-  DeviceAddress alarmAddr;
-  Serial.println("Searching for alarms...");
-  
-  // resetAlarmSearch() must be called before calling alarmSearch()
-  sensors.resetAlarmSearch();
-  
-  // alarmSearch() returns 0 when there are no devices with alarms
-  while (sensors.alarmSearch(alarmAddr))
-  {
-    Serial.print("ALARM: ");
-    printData(alarmAddr);
-  }
-*/
+  temp0 = sensors.getTempC(sondeNumero0);
+  // delay for conversion due to sensibility (i don't know where we should put this delay...)
+  delay(800);
+  Serial.print("Sonde 0 : ");
+  Serial.print(temp0);
+  Serial.print(" ");
+  temp1 = sensors.getTempC(sondeNumero1);
+  // delay for conversion due to sensibility (i don't know where we should put this delay...)
+  delay(800);
+  Serial.print("Sonde 1 : ");
+  Serial.println(temp1);
 
+  delay(10000);
 }
